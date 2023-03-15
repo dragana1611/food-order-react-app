481fd602897050e7a7d7236cf649b5af4e1f5a92
Dragana Petrovic <dragana.popov-petrovic@badin.rs>
2023-03-16 00:57:12 +0100

    first commit

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import HeaderCartButton from "./HeaderCartButton";
+import mealsImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+
+const Header = (props) => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>Meals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="meals-img" />
+      </div>
+    </>
+  );
+};
+
+export default Header;
